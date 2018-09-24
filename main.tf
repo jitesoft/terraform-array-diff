9dd4ee434dff50107b2af3b317bd94cf69f8389f
Johannes <johannes@jitesoft.com>
2018-09-24 11:38:46 +0200

    Update main.tf
    If input variable (either) is empty, set query value to empty string.

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 data "external" "array_diff" {
   program = [ "${var.shell}", "${path.module}/diff.sh" ]
   query {
-    list1 = "${join(",", var.list1)}"
-    list2 = "${join(",", var.list2)}"
+    list1 = "${length(var.list1) > 0 ? join(",", var.list1) : ""}"
+    list2 = "${length(var.list2) > 0 ? join(",", var.list2) : ""}"
   }
 }
